220d6d0b118da9818a5e09b137e53016707747eb
Richard Pilgrim <richard@assignments>
2014-08-24 20:36:58 +0800

    Added html encoding for pluses
    For some reason URL encoding and HTML encoding don't work

--- twilio.clr/Account.cs
+++ twilio.clr/Account.cs
@@ -27,7 +27,9 @@ namespace twilio.clr
             {
                 var query = vars.Cast<DictionaryEntry>().Aggregate("", (current, d) => current + ("&" + d.Key.ToString() + "=" + d.Value.ToString()));
                 if (query.Length > 0)
+                {
                     uri = uri + "?" + query.Substring(1);
+                }
             }
 
             // 2. setup basic authenication
@@ -52,6 +54,10 @@ namespace twilio.clr
             {
                 data = vars.Cast<DictionaryEntry>().Aggregate(data, (current, d) => current + (d.Key.ToString() + "=" + d.Value.ToString() + "&"));
             }
+
+            // Replace plus values in to and from numbers
+            // For some reason URL encoding and HTML encoding don't work :S
+            data = data.Replace("+", "%2B");
 
             // 2. setup basic authenication
             var authstring = Convert.ToBase64String(Encoding.ASCII.GetBytes(String.Format("{0}:{1}", _id, _token)));
